4b493e0694710f1cf4410c16708cac95c106fc2f
Yaroslav <yarolegovich@gmail.com>
2025-10-20 09:51:35 +0200

    feat: adka2a event processor logic (part 2) (#151)
    Event processing logic required for adka2a adapter.
    
    - For every `session.Event` produce a `TaskArtifactUpdateEvent{Append=true}` with transformed parts.
    - After the last `session.Event` is processed produce an empty `TaskArtifactUpdateEvent{Append=true}` with `LastChunk=true`, if at least one artifact update was produced during the run.
    - If there was an `LLMResponse` with non-zero error code, produce a `TaskStatusUpdateEvent` with `TaskStateFailed`.
    Else if there was an `LLMResponse` with long-running tool invocation, produce a `TaskStatusUpdateEvent` with `TaskStateInputRequired`.
    Else produce a `TaskStatusUpdateEvent` with `TaskStateCompleted`.

--- adka2a/metadata.go
+++ adka2a/metadata.go
@@ -16,10 +16,76 @@ package adka2a
 
 import (
 	"encoding/json"
+
+	"github.com/a2aproject/a2a-go/a2asrv"
+	"google.golang.org/adk/runner"
+	"google.golang.org/adk/session"
 )
+
+type ExecutorConfig runner.Config
+
+type invocationMeta struct {
+	//lint:ignore U1000
+	userID string
+	//lint:ignore U1000
+	sessionID string
+	eventMeta map[string]any
+}
+
+//lint:ignore U1000
+func toInvocationMeta(config *ExecutorConfig, reqCtx a2asrv.RequestContext) invocationMeta {
+	// TODO(yarolegovich): update once A2A provides auth data extraction from Context
+	userID, sessionID := "A2A_USER_"+reqCtx.ContextID, reqCtx.ContextID
+
+	m := map[string]any{
+		toMetaKey("app_name"):   config.AppName,
+		toMetaKey("user_id"):    userID,
+		toMetaKey("session_id"): sessionID,
+	}
+
+	return invocationMeta{userID: userID, sessionID: sessionID, eventMeta: m}
+}
 
 func toMetaKey(key string) string {
 	return "adk_" + key
+}
+
+func toEventMeta(meta invocationMeta, event *session.Event) (map[string]any, error) {
+	result := make(map[string]any)
+	for k, v := range meta.eventMeta {
+		result[k] = v
+	}
+
+	for k, v := range map[string]string{
+		"invocation_id": event.InvocationID,
+		"author":        event.Author,
+		"branch":        event.Branch,
+	} {
+		if v != "" {
+			result[toMetaKey(k)] = v
+		}
+	}
+
+	response := event.LLMResponse
+	if response == nil {
+		return result, nil
+	}
+
+	if response.ErrorCode != "" {
+		result[toMetaKey("error_code")] = response.ErrorCode
+	}
+
+	if response.GroundingMetadata != nil {
+		v, err := toMapStructure(response.GroundingMetadata)
+		if err != nil {
+			return nil, err
+		}
+		result[toMetaKey("grounding_metadata")] = v
+	}
+
+	// TODO(yarolegovich): include custom and usage metadata when added to session.Event
+
+	return result, nil
 }
 
 // We can't use mapstructure in a way compatible with ADK-python, because genai type fields
